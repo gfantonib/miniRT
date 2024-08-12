dc856a7c2dd36824c0b2b5159298e81fa97a6137
gfantoni <guilhermefantoniborges@gmail.com>
2024-08-12 09:35:01 -0300

    first python test added in tuple_add test.

--- include/tuple.h
+++ include/tuple.h
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 13:26:31 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/08/10 15:59:34 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/08/12 09:10:20 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,6 @@ t_tuple*	create_point(float x, float y, float z);
 t_tuple*	create_vector(float x, float y, float z);
 int			float_equal(float a, float b);
 int			tuple_equal(t_tuple a, t_tuple b);
+t_tuple*	tuple_add(t_tuple a, t_tuple b);
 
 #endif
--- tdd/04_tuple_add/main.c
+++ tdd/04_tuple_add/main.c
@@ -6,42 +6,29 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 13:31:01 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/08/10 16:11:35 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/08/12 09:23:21 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tuple.h"
 #include <stdio.h>
 
-int main(void)
+void print_tuple(t_tuple tuple)
+{
+	printf("(%f, %f, %f, %i)\n", tuple.x, tuple.y, tuple.z, tuple.w);
+}
+
+int main(int argc, char *argv[])
 {	
-	t_tuple *point1 = create_point(1.5, 2.5, 3.5);
-	t_tuple *point2 = create_point(1.5, 2.5, 3.5);
-	if (tuple_equal(*point1, *point2))
-		printf("OK\n");
-	else
-		printf("KO\n");
-		
-	t_tuple *point3 = create_point(1.5, 2.5, 3.00005);
-	t_tuple *point4 = create_point(1.5, 2.5, 3.00006);
-	if (tuple_equal(*point3, *point4))
-		printf("KO\n");
-	else
-		printf("OK\n");
-
-	t_tuple *point5 = create_point(1.5, 2.5, 3.000005);
-	t_tuple *point6 = create_point(1.5, 2.5, 3.000006);
-	if (tuple_equal(*point5, *point6))
-		printf("OK\n");
-	else
-		printf("KO\n");
-
-	t_tuple *point7 = create_point(1.5, 2.5, 3.5);
-	t_tuple *point8 = create_vector(1.5, 2.5, 3.5);
-	if (tuple_equal(*point7, *point8))
-		printf("KO\n");
-	else
-		printf("OK\n");
-		
+	if (argc != 7)
+	{
+		printf("TEST ERROR!\n");
+		return (1);
+	}
+	t_tuple *a = create_point(atof(argv[1]), atof(argv[2]), atof(argv[3]));
+	t_tuple *b = create_point(atof(argv[4]), atof(argv[5]), atof(argv[6]));
+	t_tuple *c = tuple_add(*a, *b);
+	print_tuple(*c);
+	ft_free_trashman(ft_get_mem_address());
 	return (0);
 }
