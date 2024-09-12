6b0dfe650119b7f86d6f81a68eeb58b4b362d66a
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-09-12 14:42:41 -0300

    new color functions implemented

--- source/color.c
+++ source/color.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 14:31:52 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/09/11 15:05:46 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/09/12 14:17:18 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,5 +32,12 @@ t_tuple color_color_mult(t_tuple a, t_tuple b)
 	color.x = a.x * b.x;
 	color.y = a.y * b.y;
 	color.z = a.z * b.z;
+	if (a.w == COLOR && b.w == COLOR)
+		color.w = COLOR;
+	else
+	{
+		ft_printf_fd(STDERR_FILENO, "color_color_mult() error!\n");
+		exit (1);
+	}
 	return (color);
 }
