60ef924f35ac625157d95272bde62f141dda473d
gfantoni <guilhermefantoniborges@gmail.com>
2024-09-20 18:37:41 -0300

    implementing inverse() tdd. The test is failing, but I think (almoast certain) its because the matrix_matrix_mult().

---
+++ tdd/inverse/main.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/10 13:31:01 by gfantoni          #+#    #+#             */
+/*   Updated: 2024/09/20 18:22:00 by gfantoni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tuple.h"
+#include "matrix.h"
+#include <stdio.h>
+
+void print_matrix(t_matrix matrix)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	j = 0;
+	while(i < matrix.rows)
+	{
+		j = 0;
+		while(j < matrix.columns)
+		{
+			printf("%f ", matrix.matrix[i][j]);
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+}
+
+void fill_matrix(t_matrix *matrix, char **values)
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	while(i < matrix->rows)
+	{
+		j = 0;
+		while(j < matrix->columns)
+		{
+			matrix->matrix[i][j] = atof(values[k]);
+			ft_collect_mem(values[k]);
+			j++;
+			k++;
+		}
+		i++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	t_matrix	*ma;
+	t_matrix	*mb;
+	t_matrix	*mc;
+	char		**ma_values;
+	
+	if (argc != 4)
+	{
+		printf("TEST ERROR!\n");
+		return (1);
+	}
+	ma = create_matrix(atoi(argv[1]), atoi(argv[2]));
+	ma_values = ft_split(argv[3], ' ');
+	fill_matrix(ma, ma_values);
+	ft_collect_mem(ma_values);
+	print_matrix(*ma);
+	printf("\n");
+	mb = inverse(*ma);
+	print_matrix(*mb);
+	printf("\n");
+	mc = matrix_matrix_mult(*mb, *ma);
+	print_matrix(*mc);
+	ft_free_trashman();
+	return (0);
+}
