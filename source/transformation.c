18afa4eac14dc4a4889c4eab5961dcd1b093d607
Guilherme Fantoni Borges <gfantoni@student.42sp.org.br>
2024-09-23 18:22:13 -0300

    rotations implemented but not tested.

--- source/transformation.c
+++ source/transformation.c
@@ -6,7 +6,7 @@
 /*   By: gfantoni <gfantoni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:19:18 by gfantoni          #+#    #+#             */
-/*   Updated: 2024/09/23 14:40:42 by gfantoni         ###   ########.fr       */
+/*   Updated: 2024/09/23 18:21:16 by gfantoni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,4 +39,40 @@ t_matrix *scaling(float x, float y, float z)
 	matrix->matrix[2][2] = z;
 	matrix->matrix[3][3] = 1;
 	return (matrix);
+}
+
+t_matrix *rotate_x(float radians)
+{
+	t_matrix *matrix;
+	matrix = create_matrix(4, 4);
+	matrix->matrix[0][0] = 1;
+	matrix->matrix[1][1] = cos(radians);
+	matrix->matrix[1][2] = -sin(radians);
+	matrix->matrix[2][1] = sin(radians);
+	matrix->matrix[2][2] = cos(radians);
+	matrix->matrix[3][3] = 1;
+}
+
+t_matrix *rotate_y(float radians)
+{
+	t_matrix *matrix;
+	matrix = create_matrix(4, 4);
+	matrix->matrix[0][0] = cos(radians);
+	matrix->matrix[0][2] = sin(radians);
+	matrix->matrix[1][1] = 1;
+	matrix->matrix[2][1] = -sin(radians);
+	matrix->matrix[2][2] = cos(radians);
+	matrix->matrix[3][3] = 1;
+}
+
+t_matrix *rotate_z(float radians)
+{
+	t_matrix *matrix;
+	matrix = create_matrix(4, 4);
+	matrix->matrix[0][0] = cos(radians);
+	matrix->matrix[0][1] = -sin(radians);
+	matrix->matrix[1][1] = sin(radians);
+	matrix->matrix[1][2] = cos(radians);
+	matrix->matrix[2][2] = 1;
+	matrix->matrix[3][3] = 1;
 }
